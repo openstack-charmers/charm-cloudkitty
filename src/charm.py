fb6b332db1b557e65fbcc2efb0b34457f8ed0201
hernandanielg <hernan.garcia@canonical.com>
2022-08-24 20:00:01 +0000

    added relations to mysql-router and keystone

--- src/charm.py
+++ src/charm.py
@@ -12,24 +12,39 @@ import logging
 
 import ops_openstack.core
 
-from ops.charm import CharmBase
 from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import (
+    ActiveStatus,
+    WaitingStatus
+)
 
 from charmhelpers.core import templating
 from charmhelpers.contrib.openstack import templating as os_templating
+
+from charmhelpers.contrib.openstack.ip import (
+    resolve_address
+)
+
+from charmhelpers.core.hookenv import (
+    config,
+    relation_set
+)
 
 from ops_openstack.adapters import (
     ConfigurationAdapter,
 )
 
-from charms.sunbeam_keystone_operator.v0.identity_service import IdentityServiceRequires
+from charms.sunbeam_keystone_operator.v0.identity_service import (
+    IdentityServiceRequires
+)
 
 logger = logging.getLogger(__name__)
 
+
 class CharmCloudkittyOptions(ConfigurationAdapter):
     pass
+
 
 class CharmCloudkittyCharm(ops_openstack.core.OSBaseCharm):
     """Charm the service."""
@@ -46,82 +61,100 @@ class CharmCloudkittyCharm(ops_openstack.core.OSBaseCharm):
     CONFIG_FILE_OWNER = 'cloudkitty'
     CONFIG_FILE_GROUP = 'cloudkitty'
 
+    port = '8889'
     release = 'yoga'
 
     def __init__(self, *args):
         super().__init__(*args)
 
-        self.framework.observe(self.on.install, self._on_install)
-        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.options = CharmCloudkittyOptions(self)
+
+        base_url = 'http://' + self.host + ':' + self.port
 
         self.identity_service = IdentityServiceRequires(
-            charm = self,
-            relation_name = "identity-service",
-            service_endpoints = {
-              "service_name": "cloudkitty",
-              "type": "rating",
-              "description": "Rating as a Service",
-              "internal_url": "http://10.5.1.152:8889",
-              "public_url": "http://10.5.1.152:8889",
-              "admin_url": "http://10.5.1.152:8889",
+            charm=self,
+            relation_name="identity-service",
+            service_endpoints={
+                "service_name": "cloudkitty",
+                "type": "rating",
+                "description": "Rating as a Service",
+                "internal_url": base_url,
+                "public_url": base_url,
+                "admin_url": base_url,
             },
-            region = "RegionOne"
-
-            # self, "cloudkitty",
-            # service = "rating",
-            # internal_url = "http://10.5.1.152:8889",
-            # public_url = "http://10.5.1.152:8889",
-            # admin_url = "http://10.5.1.152:8889",
-            # region = "RegionOne"
+            region=self.options.region
         )
 
-        self.framework.observe(
-            self.identity_service.on.connected, self._on_identity_service_connected)
-        self.framework.observe(
-            self.identity_service.on.ready, self._on_identity_service_ready)
-        self.framework.observe(
-            self.identity_service.on.goneaway, self._on_identity_service_goneaway)
+        self.framework.observe(self.on.install,
+                               self._on_install)
+        self.framework.observe(self.on.config_changed,
+                               self._on_config_changed)
+        self.framework.observe(self.on.shared_db_relation_joined,
+                               self._on_shared_db_relation_joined)
+
+    @property
+    def host(self):
+        return resolve_address()  # TODO: support multiple bindings
+
+    @property
+    def db_data(self):
+        relation = self.model.get_relation('shared-db')
+        if relation is None:
+            return None
+        unit = list(relation.units)[0]
+        data = relation.data[unit]
+        return {
+            'db': {
+                'user': self.options.db_user,
+                'password': data.get('password'),
+                'host': data.get('private-address'),
+                'name': self.options.db_name,
+            }
+        }
+
+    def build_context(self):
+        context = {}
+
+        db = self.db_data
+        if db is not None:
+            context.update(db)
+
+        context.update({
+            'options': self.options,
+            'identity_service': self.identity_service
+        })
+
+        return context
 
     def _on_install(self, event):
         super().on_install(event)
 
-    def _on_config_changed(self, event):
-         templating.render(
+    def _on_config_changed(self, _):
+        if self.options.debug:
+            import ripdb; ripdb.set_trace()
+
+        templating.render(
             source='cloudkitty.conf',
-            template_loader=os_templating.get_loader('templates/',
-                                                     self.release),
+            template_loader=os_templating.get_loader(
+                'templates/',
+                self.release
+            ),
             target='/etc/cloudkitty/cloudkitty.conf',
-            context={'options': CharmCloudkittyOptions(self)},
+            context=self.build_context(),
             owner=self.CONFIG_FILE_OWNER,
             group=self.CONFIG_FILE_GROUP,
-            perms=0o440
+            perms=0o640
         )
+        self.unit.status = ActiveStatus()
 
-    def _on_identity_service_connected(self, _):
-        '''React to the IdentityService connected event.
+    def _on_shared_db_relation_joined(self, _):
+        relation_set(
+            database=self.options.db_name,
+            username=self.options.db_user,
+            hostname=resolve_address()
+        )
+        self.unit.status = WaitingStatus()
 
-        This event happens when n IdentityService relation is added to the
-        model before credentials etc have been provided.
-        '''
-        # Do something before the relation is complete
-        pass
-
-    def _on_identity_service_ready(self, _):
-        '''React to the IdentityService ready event.
-
-        The IdentityService interface will use the provided config for the
-        request to the identity server.
-        '''
-        # IdentityService Relation is ready. Do something with the completed relation.
-        pass
-
-    def _on_identity_service_goneaway(self, _):
-        '''React to the IdentityService goneaway event.
-
-        This event happens when an IdentityService relation is removed.
-        '''
-        # IdentityService Relation has goneaway. shutdown services or suchlike
-        pass
 
 if __name__ == "__main__":
     main(CharmCloudkittyCharm)
